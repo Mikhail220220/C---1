55beaca448cb8e1f1d20a1653f9c543fb07b4d6d
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-19 12:07:21 +0600

    task2 finish

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter first number: ");
+int nub1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Enter second number: ");
+int nub2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number: ");
+int nub3 = Convert.ToInt32(Console.ReadLine());
+int Max = 0;
+    if (nub1 < nub2) 
+        Max = nub2;
+    
+    if (nub2 < nub3)
+        Max = nub3;
+    
+    
+    if (nub3 < nub1)    
+        Max = nub1;
+
+Console.WriteLine(Max);
